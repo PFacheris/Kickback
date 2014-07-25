b0a6b155402ced8c620650de401b51f172b49284
Patrick Facheris <plfacheris@gmail.com>
2014-07-25 17:20:51 -0400

    add call-to-action explanation in footer

--- static_dev/scripts/main.js
+++ static_dev/scripts/main.js
@@ -1,1 +1,14 @@
-console.log('\'Allo \'Allo!');
+$(function() {
+  var sizeCallToActionExplanation;
+  (sizeCallToActionExplanation = function() {
+    var $items = $('.call-to-action-explanation > li');
+    $items.css('height', 'auto');
+    var max = 0;
+    $items.each(function() {
+      var thisHeight;
+      if ((thisHeight = $(this).height()) > max) { max = thisHeight; }
+    }).height(max);
+  })();
+
+  $(window).on('resize', sizeCallToActionExplanation);
+});
