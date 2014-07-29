b1e8c2f42e05447c3b479ec72cd5756098f8ed0f
Matt Condon <MattCMultimedia@gmail.com>
2014-07-29 15:31:04 -0400

    yay, unmarshalling and db queries\!

--- tasks/updateProducts/main.go
+++ tasks/updateProducts/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
-	"github.com/DDRBoxman/go-amazon-product-api"
-	"github.com/pfacheris/kickback/config"
-	"github.com/pfacheris/kickback/tasks/lib/emailparser"
+	"github.com/Shrugs/goaws/productadvertising/v1"
+	"github.com/kr/pretty"
 	"math"
 	"sync"
 	"time"
+
+	// app
+	"github.com/Shrugs/go-amazon-product-api"
+	"github.com/pfacheris/kickback/config"
+	. "github.com/pfacheris/kickback/db"
+	"github.com/pfacheris/kickback/models"
 )
 
 func main() {
@@ -22,40 +28,68 @@ func main() {
 	// grab all of the products who's ScrapedAt date is older than a day ago
 	aDayAgo := time.Now().AddDate(0, 0, -1)
 
-	wg := sync.WaitGroup()
-	wg.Add(len(products))
+	products := []models.Product{}
+	DB.Where("scraped_at < ?", aDayAgo).Find(&products)
 
+	productIDs := make([]string, len(products))
 	for _, product := range products {
-		go func(product *kickback.Product, wg *sync.WaitGroup) {
+		productIDs = append(productIDs, product.ProductId)
+	}
+
+	setOfProducts := splitSlice(productIDs, 10)
+
+	var wg sync.WaitGroup
+	wg.Add(len(setOfProducts))
+
+	for _, someProducts := range setOfProducts {
+		go func(someProducts []string, wg *sync.WaitGroup) {
 			defer wg.Done()
 
 			// foreach product, grab its info from the Amazon Product API
-			result, err := api.ItemSearchByKeyword("sgt+frog")
+			result, err := api.MultipleItemLookup(someProducts)
 			if err != nil {
 				fmt.Println(err)
 			}
 
-			fmt.Println(result)
+			var response productadvertising.ItemLookupResponse
+			xml.Unmarshal([]byte(result), &response)
 
-		}(product, &wg)
+			for _, item := range response.Items {
+				var product models.Product
+				DB.Where("product_id = ?", item.ASIN).First(&product)
+
+				pretty.Println(item.OfferSummary)
+				pretty.Println(item.Offers)
+
+				// product.CurrentPrice =
+			}
+
+			// update the product price in the db
+			// now for every product, get purchases
+			// for each purchase
+			// KickbackAmount = originalPrice - currentPrice
+			// if KickbackAmount is posistive, yay, schedule an email and mark as kickbacked
+			// else, do nothing
+
+		}(someProducts, &wg)
 	}
-
+	fmt.Println("Waiting...")
 	wg.Wait()
+	fmt.Println("Done...")
 }
 
 func splitSlice(ids []string, idsPerSplit int) (ret [][]string) {
 
-	numIter := math.Ceil(len(ids) / idsPerSplit)
+	numIter := int(math.Ceil(float64(len(ids)) / float64(idsPerSplit)))
 
-	// @TODO fix this
-	ret = make([][]string)
-
-	for i := 1; i <= numIter; i++ {
-		ret[i-1] = ids[(i-1)*idsPerSplit : i*idsPerSplit]
+	ret = make([][]string, numIter)
+	for i := 0; i < numIter; i++ {
+		ret[i] = make([]string, idsPerSplit)
 	}
 
-	// for (var i = 0; i < numIter; i++) {
-	//     // take the first <itemsPerRow> and add to a row
-	//     r += rowTemplate({projects: ps.splice(0, itemsPerRow)});
-	// }
+	for i := 1; i <= numIter; i++ {
+		ret[i-1] = ids[(i-1)*idsPerSplit : int(math.Min(float64(i*idsPerSplit), float64(len(ids))))]
+	}
+
+	return
 }
