b905d2b85eba9edf9485549a0ac3167812b8b6de
Patrick Facheris <plfacheris@gmail.com>
2014-07-29 11:52:38 -0400

    initial framework of dashboard page

--- controllers/errorController.go
+++ controllers/errorController.go
@@ -1,22 +1,22 @@
 package controllers
 
 import (
-  "github.com/martini-contrib/render"
+	"github.com/martini-contrib/render"
 )
 
 func HandleError(encoding string, status int, e error, r render.Render) {
-  reportError(e)
+	reportError(e)
 
-  switch encoding {
-  case "json":
-    r.JSON(status, map[string]string{
-      "message": e.Error(),
-    })
-  default:
-    r.HTML(status, "error", map[string]string{
-      "message": e.Error(),
-    })
-  }
+	switch encoding {
+	case "json":
+		r.JSON(status, map[string]string{
+			"message": e.Error(),
+		})
+	default:
+		r.HTML(status, "error", map[string]string{
+			"message": e.Error(),
+		})
+	}
 }
 
 // @TODO: Error reporting service and/or logging.
