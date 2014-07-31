64bf1c9bb4b35b9d67a0d8611c129b111cfef79c
Matt Condon <MattCMultimedia@gmail.com>
2014-07-31 14:07:04 -0400

    better debug

--- tasks/updatePurchases/main.go
+++ tasks/updatePurchases/main.go
@@ -138,7 +138,7 @@ func main() {
 							r, _ := base64.URLEncoding.DecodeString(b64body)
 							return strings.NewReader(string(r[:])), err
 						}
-						pretty.Println(r)
+						pretty.Println(b64body)
 						return strings.NewReader(""), errors.New("NO EMAIL BODY???")
 					}()
 
